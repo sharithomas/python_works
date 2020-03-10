ebb773543a1b146553457730aee1d7f6aae07ec8
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-10 15:18:25 +0530

    Add files via upload

---
+++ multiplies_for.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 15:19:38 2020
+
+@author: SHARI
+"""
+
+#Print all 3 multiples from 1 to 100 using for loop.
+for i in range(1,101):
+    r=i*3
+    print("{}*3=".format(i),r,"\n")
