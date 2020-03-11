e758d22343130ef0795dc8e95fa6be958cd800cc
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-11 15:02:53 +0530

    Add files via upload

---
+++ square_list.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 14:15:09 2020
+
+@author: SHARI
+"""
+
+#Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+# Then the function needs to print the last 5 elements in the list.
+n=int(input("enter limit"))
+square_list=[]
+
+def square_list():
+    for x in range(1,n+1):
+        square_list.append(x*x)
+    print(square_list[-5:])
+    
+print("last 5 elemnts from the square list",square_list())
+    
