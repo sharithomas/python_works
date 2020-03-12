6c8eeb31e07e319c6cad3c7d17083bb09dfb82e1
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-12 12:11:45 +0530

    Add files via upload

---
+++ random_float.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 09:49:17 2020
+
+@author: SHARI
+"""
+
+#Please generate a random float where the value is between 10 and 100 using Python math module.
+import random
+print("random float number between 10 and 100")
+random.uniform(10,100)
