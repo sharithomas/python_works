e46eb89983977b27d6ae4f3ef0618514bbe4c32b
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-12 14:12:53 +0530

    Add files via upload

---
+++ unicode_string.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 14:10:44 2020
+
+@author: SHARI
+"""
+
+#Print a unicode string "hello world".
+
+aa = u"hello world"  # unicode string starts with “u”
+
+print (aa) # I ♥ U
