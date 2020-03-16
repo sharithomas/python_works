25cb5b2862b1cff94123865c2e0eb248eb1894fa
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-16 18:10:15 +0530

    Update even_tuples.py

--- even_tuples.py
+++ even_tuples.py
@@ -8,7 +8,7 @@ Created on Mon Mar 16 17:59:39 2020
 #Write a program to generate and print another tuple whose values are even numbers in the given tuple (1,2,3,4,5,6,7,8,9,10)
 tuple_input=(1,2,3,4,5,6,7,8,9,10)
 #even numbers  from the given tuple
-tuple_output=(x for x in tuple_input if x%2==0)
+tuple_output=tuple(x for x in tuple_input if x%2==0)
 print(" all even numbers from the given tuple")
 for i in tuple_output:
     print(i,end=",")
