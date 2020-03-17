935232740bceb7e9cf5ef5f4bd4a71c7c42288fc
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-17 08:44:08 +0530

    Add files via upload

---
+++ a_aa_aaa_aaaa.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 10:13:54 2020
+
+@author: SHARI
+"""
+#write a program that computes the values of a+aa+aaa+aaaa with a given digit as value of a
+a=input("enter a number")
+b=a*2
+c=a*3
+d=a*4
+print("value of a in a+aa+aaa+aaaa format")
+print(a+b+c+d)
