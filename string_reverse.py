0134b9630d6f23600149122ee697b388135dc870
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-17 13:00:36 +0530

    Add files via upload

---
+++ string_reverse.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:56:03 2020
+
+@author: SHARI
+"""
+
+#Please write a program which accepts a string from console and print it in reverse order. 
+#Example: If the following string is given as input to the program: rise to vote sir
+# Then, the output of the program should be: ris etov ot esir
+string_in=input("enter string")
+#reverse order of string
+print("string in reverse order:",string_in[::-1])
