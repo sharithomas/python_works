6d1603e677bee9ee6ba8f0de6cb19c4365609bb9
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-18 07:35:48 +0530

    Add files via upload

---
+++ random_float1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 07:33:31 2020
+
+@author: SHARI
+"""
+
+#Please generate a random float where the value is between 5 and 95 using Python math module.
+import random
+print("random float number between 5 and 95")
+random.uniform(5,95)
