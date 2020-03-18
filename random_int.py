964b2fa42b0c567b2d818ab60467093e0623c627
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-18 12:04:05 +0530

    Add files via upload

---
+++ random_int.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 12:00:16 2020
+
+@author: SHARI
+"""
+
+#Please write a program to randomly print a integer number between 7 and 15 inclusive.
+import random
+print(random.randint(7,15))
