13a78e903e3bcbd384b7e277ccda8c7cd5c42b25
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-03-19 08:31:20 +0530

    Add files via upload

---
+++ random_list_5even.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 08:21:17 2020
+
+@author: SHARI
+"""
+
+#Please write a program to randomly generate a list with 5 even numbers between 100 and 200 inclusive.
+import random
+count=0
+list_out=[]
+while count<=5:
+    num=random.randint(100,200)
+    if num%2==0:
+        list_out.append(num)
+    else:
+        list_out.append(num+1)
+    count=count+1
